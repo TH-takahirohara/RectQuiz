a0488844f50cff9d1e4c896a9e80c7a812341416
Takahiro Hara <takahiro.hb@gmail.com>
2020-10-18 03:01:10 +0900

    Initial Commit

---
+++ RectQuiz/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RectQuiz
+//
+//  Created by 原昂大 on 2020/10/18.
+//  Copyright © 2020 takahiro.hara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RectQuiz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RectQuiz
+//
+//  Created by 原昂大 on 2020/10/18.
+//  Copyright © 2020 takahiro.hara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
